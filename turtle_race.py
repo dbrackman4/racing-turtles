d722f6068cee8acbe46d28fd214283fa4ca486f9
Drew Brackman <brackmandrew@gmail.com>
2024-06-20 16:58:26 -0600

    Updated turtle race track and movements

--- turtle_race.py
+++ turtle_race.py
@@ -6,6 +6,7 @@ num_turtles = 8
 turtles = []
 speeds = []
 colors = ['red', 'yellow', 'blue', 'green', 'magenta', 'brown', 'purple', 'lime']
+names = ["Big Red", "Banana", "Sapphire", "The Machine", "Bubblegum", "Desmond", "Violet", "Skittle"]
 screen = turtle.Screen()
 drawTurtle = turtle.Turtle()
 drawTurtle.hideturtle()
@@ -20,25 +21,41 @@ def drawSquare(t, length):  #Draws a square of length "length" with turtle "t" w
     drawTurtle.end_fill()
 
 def drawFinishLine():
-    drawTurtle.goto(-375, 425)
+    drawTurtle.goto(-375, 500)
     drawTurtle.right(90)
+
+    drawTurtle.fillcolor("white")
+    drawTurtle.begin_fill()
+    for _ in range(2):
+        drawTurtle.forward(750)
+        drawTurtle.right(90)
+        drawTurtle.forward(50)
+        drawTurtle.right(90)
+    drawTurtle.end_fill()
+    drawTurtle.fillcolor("black")
     for _ in range(15):
-        drawTurtle.fillcolor("black")
         drawSquare(drawTurtle, 25)
         drawTurtle.forward(25)
         drawTurtle.right(90)
         drawTurtle.forward(25)
         drawTurtle.left(90)
         drawSquare(drawTurtle, 25)
-        drawTurtle.fillcolor("white")
-        drawTurtle.backward(25)
-        drawSquare(drawTurtle, 25)
         drawTurtle.forward(25)
         drawTurtle.left(90)
         drawTurtle.forward(25)
         drawTurtle.right(90)
-        drawSquare(drawTurtle, 25)
-        drawTurtle.forward(25)
+
+def drawGround():
+    drawTurtle.fillcolor("tan")
+    drawTurtle.goto(-375, 450)
+    drawTurtle.begin_fill()
+    for _ in range(2):
+        drawTurtle.forward(750)
+        drawTurtle.right(90)
+        drawTurtle.forward(950)
+        drawTurtle.right(90)
+    drawTurtle.end_fill()
+
     
 def initializeTurtles():
     
@@ -47,11 +64,13 @@ def initializeTurtles():
         turtles.append(turtle.Turtle())
         #set turtle "speeds"
         turtles[i].speed(10)
-        speeds.append(random.randint(1, 5))
+        speeds.append(random.uniform(0, 5))
         #set turtle shape to turtle
         turtles[i].shape("turtle")
         #set turtle colors
         turtles[i].color(colors[i])
+        #set turtle names
+        setattr(turtles[i], "name", names[i])
         #set starting positions
         turtles[i].penup()
         turtles[i].goto(-350 + (i) * 100, -400)
@@ -62,12 +81,12 @@ def raceTurtles():
     finished = False
     while not finished:
         for i in range(num_turtles):
-            turtles[i].forward(speeds[i])
+            turtles[i].forward(speeds[i] + random.uniform(0, 5))
             if checkEndCondition(i):
                 finished = True
 
 def checkEndCondition(i):
-    if turtles[i].ycor() >= 400:
+    if turtles[i].ycor() >= 475:
         return True
     return False
 
@@ -75,12 +94,13 @@ def checkEndCondition(i):
 def main():
     screen.screensize(1000, 1000)
     screen.bgcolor("light gray")
-    drawFinishLine()
 
+    drawFinishLine()
+    drawGround()
     initializeTurtles()
-    
-    
     raceTurtles()
+
     screen.exitonclick()
     
+
 main()
